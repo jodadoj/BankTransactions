e526088a1370e7a96a287cee7c62baf47d9d96a9
Josiah Adojutelegan <JosiahAdojutelegan@gmail.com>
2023-09-08 14:23:10 +0100

    Successfully implemented secrets file to properly hold sensitive data

--- Program.cs
+++ Program.cs
@@ -1,7 +1,21 @@
 using BankTransactions.Models;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 var builder = WebApplication.CreateBuilder(args);
+//take connection string from secrets.json in full
+//var connectString = builder.Configuration["DEVCONNECTION:FULL"];
+
+//take connection string from appsettings piece by piece
+var conStrBuilder = new NpgsqlConnectionStringBuilder(
+        builder.Configuration.GetConnectionString("DevConnection"));
+conStrBuilder.Host = builder.Configuration["DEVCONNECTION:SERVER"];
+conStrBuilder.Database = builder.Configuration["DEVCONNECTION:DATABASE"];
+conStrBuilder.Port = Int32.Parse(builder.Configuration["DEVCONNECTION:PORT"]);
+conStrBuilder.Username = builder.Configuration["DEVCONNECTION:USERNAME"];
+conStrBuilder.Password = builder.Configuration["DEVCONNECTION:PASSWORD"];
+var connection = conStrBuilder.ConnectionString;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -9,7 +23,9 @@ builder.Services.AddControllersWithViews();
 //DI for DbContext
 builder.Services.AddEntityFrameworkNpgsql().AddDbContext<TransactionDbContext>(options =>
 {
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection"));
+    //options.UseNpgsql(builder.Configuration.GetConnectionString("DevConnection"));
+    //options.UseNpgsql(connectString);
+    options.UseNpgsql(connection);
 });
 
 
